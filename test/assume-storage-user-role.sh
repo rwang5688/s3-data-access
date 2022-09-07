fdf201aa7fc1c93c10e8b8cd9eba3dec58d78db1
Robert Wang <wangrob@amazon.com>
2022-09-06 18:15:08 -0700

    fix misspelling (should be storage, not stroage)

--- test/assume-storage-user-role.sh
+++ test/assume-storage-user-role.sh
@@ -2,7 +2,7 @@
 export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 $(aws sts assume-role \
 --profile default \
---role-arn arn:aws:iam::123456789012:role/stroage-user-role \
+--role-arn arn:aws:iam::123456789012:role/storage-user-role \
 --role-session-name storage-user-role-session \
 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
 --output text))
